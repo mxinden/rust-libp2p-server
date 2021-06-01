b3e722eb093c9ebf7984ab502ec3c2d14eba57b9
Max Inden <mail@max-inden.de>
2021-06-01 08:49:01 +0200

    src/metric_server: Change OpenMetrics port to 8888
    This is the default Prometheus port of go-ipfs.

--- src/metric_server.rs
+++ src/metric_server.rs
@@ -17,7 +17,7 @@ pub async fn run(registry: Registry) -> std::result::Result<(), std::io::Error> 
             Ok(String::from_utf8(encoded).unwrap())
         });
 
-    let listen_addr = "0.0.0.0:8080";
+    let listen_addr = "0.0.0.0:8888";
     println!("Listening for metric requests on {}/metrics", listen_addr);
     app.listen(listen_addr).await?;
 
